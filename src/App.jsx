52b846ca807bd2cb14c76956b9852acd34ce36e9
seantai <seantai@protonmail.com>
2022-10-25 17:22:49 +0200

    .
    bump versions, delete index.css, update bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import './css/App.css'
-import { useEffect, useState, useRef } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, RoundedBox, MeshWobbleMaterial } from '@react-three/drei'
-import { Foo } from './components/Foo'
+import "./css/App.css";
+import { useEffect, useState, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  OrbitControls,
+  RoundedBox,
+  MeshWobbleMaterial,
+} from "@react-three/drei";
+import { Foo } from "./components/Foo";
 
 const Scene = () => {
-
   return (
     <>
-    <RoundedBox>
-      <MeshWobbleMaterial wireframe color={'hotpink'}/>
-    </RoundedBox>
-    <ambientLight />
-    <OrbitControls />
+      <RoundedBox>
+        <MeshWobbleMaterial wireframe color={"#ddfcd8"} />
+      </RoundedBox>
+      <ambientLight />
+      <OrbitControls />
     </>
-  )
-}
+  );
+};
 
 export default function App() {
   return (
-    <Canvas camera={{position: [0,0,2]}}>
+    <Canvas camera={{ position: [0, 0, 2] }}>
       <Scene />
     </Canvas>
-  )
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './css/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
