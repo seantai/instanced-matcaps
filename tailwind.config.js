3c2b6b07d50ed53e79c80d5ee83791f6743182bf
seantai <seantai@protonmail.com>
2024-01-19 18:49:43 +0100

    add tailwind font var

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   // content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        antonio: ['Antonio', 'sans-serif']
+      }
+    }
   },
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  plugins: ['prettier-plugin-tailwindcss']
+}
