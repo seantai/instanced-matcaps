eb9de45237c3879fc4e550f78f37cce7685dd49a
seantai <seantai@protonmail.com>
2024-01-19 18:54:20 +0100

    add cursor cam movement

--- src/components/Kamera.jsx
+++ src/components/Kamera.jsx
@@ -1,42 +1,23 @@
-import { useEffect, useRef } from "react";
-import { CameraControls } from "@react-three/drei";
-import { store } from "../store";
+import { useEffect, useRef } from 'react'
+import { CameraControls } from '@react-three/drei'
+import { store } from '../store'
+import { useFrame } from '@react-three/fiber'
+import { Vector3 } from 'three'
 
 export const Kamera = () => {
-  const camRef = useRef();
+  const camRef = useRef()
 
   useEffect(() => {
-    store.loaded = true;
+    store.loaded = true
+  }, [])
 
-    const handleKeyDown = (event) => {
-      if (event.key === "k") {
-        console.log(event);
-        console.log(camRef.current.camera.position);
-        console.log(camRef.current.azimuthAngle);
-        console.log(camRef.current.polarAngle);
-      }
-    };
+  const targetPosition = new Vector3()
+  useFrame(({ pointer }) => {
+    if (camRef.current) {
+      targetPosition.set(-Math.sin(pointer.x), -Math.atan(pointer.y), 4)
+      camRef.current.setPosition(targetPosition.x, targetPosition.y, targetPosition.z, true)
+    }
+  })
 
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
-  return (
-    <CameraControls
-      ref={camRef}
-      makeDefault
-      // polarAngle={1.9412172877096}
-      // azimuthAngle={-0.4080060034431936}
-      // mouseButtons={{ left: 0, middle: 0, right: 0, wheel: 0 }}
-      // touches={{ one: 0, two: 0, three: 0 }}
-      // minAzimuthAngle={-0.2}
-      // maxAzimuthAngle={0.2}
-      // minPolarAngle={1}
-      // maxPolarAngle={1.3}
-      // smoothTime={0.7}
-    />
-  );
-};
+  return <CameraControls ref={camRef} makeDefault />
+}
