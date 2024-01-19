8cccbd674e6bc6a801414ed92ebca33bd551bc09
seantai <seantai@protonmail.com>
2024-01-19 18:21:04 +0100

    add overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
 import { Canvas } from '@react-three/fiber'
-import { Bvh } from '@react-three/drei'
-import { InstancedMesh } from './components/InstancedMesh'
+import { Bvh, Environment, useGLTF, useTexture } from '@react-three/drei'
+import { useRef } from 'react'
 import { Kamera } from './components/Kamera'
-import { Perf } from 'r3f-perf'
+import { Overlay } from './components/Overlay'
+// import { Perf } from 'r3f-perf'
 
 const Scene = () => {
+  const Suz = useGLTF('./Suz_Quad.glb')
+  const matcapTexture = useTexture('161B1F_C7E0EC_90A5B3_7B8C9B.png')
   return (
     <>
-      <InstancedMesh />
+      <mesh geometry={Suz.nodes.Suz_Quad.geometry}>
+        <meshMatcapMaterial matcap={matcapTexture} />
+      </mesh>
+
       <Kamera />
-      {/* <ambientLight intensity={0.6} /> */}
+
+      {/* <Environment files={'neutral.hdr'} /> */}
     </>
   )
 }
 
 export default function App() {
+  const parent = useRef()
+
   return (
-    <Canvas camera={{ position: [20, -20, 30] }}>
-      <Bvh firstHitOnly>
-        <Scene />
-      </Bvh>
-      {/* <Perf /> */}
-    </Canvas>
+    <div className="w-full h-full relative cursor-pointer" ref={parent}>
+      <Overlay />
+      <Canvas shadows camera={{ position: [0, 2, 3] }} eventSource={parent.current}>
+        <Bvh firstHitOnly>
+          <Scene />
+        </Bvh>
+        {/* <Perf /> */}
+      </Canvas>
+    </div>
   )
 }
---
+++ src/components/Overlay.jsx
@@ -0,0 +1,41 @@
+export function Overlay() {
+  return (
+    <>
+      <div className="absolute top-0 left-0 w-full h-full p-4 md:p-10 flex items-start flex-col  select-none overflow-hidden">
+        <div className="flex w-full p-0 items-center justify-between">
+          <p className="font-antonio text-5xl tracking-[-2px] font-bold">3D WEB</p>
+          <p className="text-3xl">⍫</p>
+        </div>
+        <div className="mt-16 w-full p-0 flex items-start justify-between">
+          <div className="flex flex-col h-full text-sm">
+            <p className="font-bold">R3F, Vite</p>
+            <p>Interactive technology</p>
+            <p>—</p>
+          </div>
+          <p
+            className="transform rotate-90 translate-x-full translate-y-2 origin-right text-md font-bold leading-normal text-right whitespace-nowrap"
+            style={{
+              transform: 'rotate3d(0, 0, 1, 90deg) translate3d(100%,10px,0)'
+            }}>
+            DRAG POINTER &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ●
+          </p>
+        </div>
+        <div className="font-antonio w-full h-full flex justify-between items-end p-0">
+          <p className="text-[100px] md:text-[250px] leading-[100%]">ߥ</p>
+          <p className="text-[100px] md:text-[250px] leading-[100%]">ﮯ1</p>
+        </div>
+        <div className="mt-14 w-full flex items-end justify-between p-0">
+          <div className="text-md flex flex-col">
+            <p className="font-semibold">Dreaming of Shaders</p>
+            <p>Dreaming of Shaders</p>
+          </div>
+
+          <p className="justify-self-end  text-md">Berlin, San Francisco</p>
+        </div>
+      </div>
+      <div className="left-0 right-0 fixed mb-24 md:mb-14 text-center bottom-0 font-semibold tracking-tighter text-xl ">
+        <p className="cursor-pointer select-none">CLICK SOMEWHERE</p>
+      </div>
+    </>
+  )
+}
